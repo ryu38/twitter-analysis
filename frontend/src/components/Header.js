5c631f69a7623be276440149ab90886ad992459d
ryu38 <niceneetlife.ryu@gmail.com>
2020-04-08 14:30:14 +0900

    add description

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,15 @@ import './Header.scss'
 
 const Header = props => {
     return (
-        // <div className={'header'}>
-            <h1 className={'title'} style={{display: props.displayIndex == -1 ? "": "none" }}>Splatter</h1>
-        // </div>
+        <div className={'header'} style={{display: props.displayIndex == -1 ? "": "none" }}>
+            {/*<h1 className={'title'} style={{display: props.displayIndex == -1 ? "": "none" }}>Splatter</h1>*/}
+            <div className={'logo'}>
+                <img src="https://fontmeme.com/permalink/200408/77701b9d03a603fd471c47c958551652.png" alt="protocol-font" border="0"/>
+            </div>
+            <div className={'dsc'}>
+                <p>twitterで人気が高いスプラトゥーン2のイカした動画を一挙ご紹介！</p>
+            </div>
+        </div>
     )
 };
 
